7492ab5687416ae38beeb22211cd9e9ff712aa25
Harsh Kumar gupta <hkg8558@gmail.com>
2024-12-31 17:49:46 +0530

    add

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -1,4 +1,4 @@
-import logo from "../public/logo.png";
+//import logo from "../public/logo.png";
 const Loading = () => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black  z-50">
@@ -7,7 +7,7 @@ const Loading = () => {
         <div className="absolute inset-0 w-28 h-28 rounded-full border-4 border-transparent border-t-red-500 border-b-gray-400 animate-glow"></div>
         {/* Pulsing Logo */}
         <img
-          src={logo}
+          src='https://res.cloudinary.com/dxmu1ohyk/video/upload/f_auto:video,q_auto/v1/gym_web/jkzb7pbyxti3i2nqkzuq'
           alt="Loading"
           className="relative w-28 h-28 animate-pulse-shadow"
         />
